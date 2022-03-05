ceecf0ad46c47fd6cced7a76496373bfd7e7a912
mcombeau <52008667+mcombeau@users.noreply.github.com>
2022-03-05 16:30:35 +0100

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -9,7 +9,7 @@ RAM_PERC=$(free -k | grep Mem | awk '{printf("%.2f%%"), $3 / $2 * 100}')
 DISK_TOTAL=$(df -h --total | grep total | awk '{print $2}')
 DISK_USED=$(df -h --total | grep total | awk '{print $3}')
 DISK_PERC=$(df -k --total | grep total | awk '{print $5}')
-CPU_LOAD=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f"), $1 + $3}')
+CPU_LOAD=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
 LAST_BOOT=$(who -b | awk '{print($3 " " $4)}')
 LVM=$(if [ $(lsblk | grep "lvm" | wc -l) -eq 0 ]; then echo no; else echo yes; fi)
 TCP=$(grep TCP /proc/net/sockstat | awk '{print $3}')
@@ -18,14 +18,13 @@ IP_ADDR=$(hostname -I | awk '{print $1}')
 MAC_ADDR=$(ip link show | grep link/ether | awk '{print $2}')
 SUDO_LOG=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
 
-wall  "
-       ------------------------------------------------
+echo  " ------------------------------------------------
        #Architecture  : $ARCH
        #Physical CPUs : $PCPU
        #Virtual CPUs  : $VCPU
        #Memory Usage  : $RAM_USED/$RAM_TOTAL ($RAM_PERC)
        #Disk Usage    : $DISK_USED/$DISK_TOTAL ($DISK_PERC)
-       #CPU Load      : $CPU_LOAD%
+       #CPU Load      : $CPU_LOAD
        #Last Boot     : $LAST_BOOT
        #LVM use       : $LVM
        #TCP Connections: $TCP ESTABLISHED
